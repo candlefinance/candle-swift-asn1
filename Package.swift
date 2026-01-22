cacb60abc0ca73100f6a42f9e709aaf1e4c9d970
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 05:52:05 -0500

    prefixing candle- to package name

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let upcomingFeatureSwiftSettings: [SwiftSetting] = [
 ]
 
 let package = Package(
-    name: "swift-asn1",
+    name: "candle-swift-asn1",
     products: [
         .library(name: "SwiftASN1", targets: ["SwiftASN1"])
     ],
